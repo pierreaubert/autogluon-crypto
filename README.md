9e5e8e8a49e6d115647dcfdec776e8654a169175
pierreaubert <1709343+pierreaubert@users.noreply.github.com>
2024-02-22 12:48:45 +0100

    add a README and some .gitignore

---
+++ README.md
@@ -0,0 +1,21 @@
+# Playing with the autogluon library on some cryptos data
+
+## Installation
+
+On Linux with an NVIDIA GPU, just run:
+```
+> python3 -m venv .env
+> source .env/bin/activate
+> sh requirements.sh
+```
+
+Take a coffee. If you are not on Linux, look at the [documentation](https://auto.gluon.ai/stable/install.html) for autogluon.
+
+## Start the notebook
+
+```
+jupyter-lab & 
+```
+
+and play with it.
+
--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "you should create a virtual env:"
+echo "python3 -m venv .env"
+echo "source .env/bin/activate"
+
 pip install -U pip
 pip install -U setuptools wheel
 
@@ -7,3 +11,6 @@ Install the proper version of PyTorch following https://pytorch.org/get-started/
 pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --index-url https://download.pytorch.org/whl/cu118
 
 pip install autogluon
+
+# needs to be after autogluon and this will generates some conflicts
+pip install jupyter ipywidgets
